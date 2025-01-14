2eb0ec0a37f0c55cd481f1c2a9532c2b8dea7412
천성윤 <1112csy@naver.com>
2025-01-14 17:03:30 +0900

    init: podfile setting

--- index.js
+++ index.js
@@ -1,9 +1,5 @@
-/**
- * @format
- */
-
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
+import { AppRegistry } from "react-native";
+import App from "./src/App"; // App 컴포넌트 경로 확인
+import { name as appName } from "./app.json";
 
 AppRegistry.registerComponent(appName, () => App);
